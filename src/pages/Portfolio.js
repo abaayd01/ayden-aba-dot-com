515d26135e5844dd65ea2076460a8b0effab553a
Ayden <abaayd01@gmail.com>
2018-07-05 21:54:55 +0930

    foundation for building out html structure

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const Portfolio = () => (
+  <div>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default Portfolio;
